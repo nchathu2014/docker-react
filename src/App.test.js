77f0d29760d7f9bca9ad1f717831fd47376c8626
Chathuranga <Nuwan.Chathuranga@pearson.com>
2021-03-07 01:17:33 +0530

    [TEST]:Revert back after tested

--- src/App.test.js
+++ src/App.test.js
@@ -10,7 +10,7 @@ test('renders learn react link', () => {
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react12/i);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
 
