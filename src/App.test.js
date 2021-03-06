4fbc6e986ca75fa8ef276b7b11ca3fc213b6b8c7
Chathuranga <Nuwan.Chathuranga@pearson.com>
2021-03-07 01:13:24 +0530

    [Test]: Check the test scripts by adding a fail test case

--- src/App.test.js
+++ src/App.test.js
@@ -10,7 +10,7 @@ test('renders learn react link', () => {
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/learn react1/i);
   expect(linkElement).toBeInTheDocument();
 });
 
